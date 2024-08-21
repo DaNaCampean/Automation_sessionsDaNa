a874a5751d86b8743673ce201f4cfeac00f2a9f7
DaNaCampean <campean.dana@gmail.com>
2024-08-21 14:46:42 +0300

    new PROBLEM

---
+++ src/pROBLEM.java
@@ -0,0 +1,45 @@
+public class pROBLEM {
+
+    /*
+
+    Ticket Sales Monitor
+The busiest visiting period for a local attraction is approaching, and the staff needs help
+managing ticket sales and calculating the weekly balance. The ticket price varies depending
+on the park's visiting schedule, specifically on the time slot. Students receive a 75%
+discount, seniors a 50% discount, and children enter for free.
+Visiting program:
+Monday: 09:00 - 18:00
+Tuesday: 11:00 - 19:00
+Wednesday: 08:00 - 17:00
+Thursday: 09:00 - 20:00
+Friday: 10:00 - 18:00.
+Ticket Prices:
+08:00 - 12:00 | Price: $10.0
+12:00 - 17:00 | Price: $20.0
+17:00 - 20:00 | Price: $30.0
+● Implement the visiting timetable of the local attraction using a Map implementation of
+your choice in which the keys are days of the week and the values are time intervals
+(opening and closing hours).
+● Create a boolean method called isOpen that will take as parameters the day of the
+week and the time of the arrival (e.g. hour) and returns true if the local attractions is
+open on that time or false otherwise.
+● Create a method that calculates and returns the price based on the type of ticket
+(child, student, adult, or senior) and the time at which it is purchased.
+● Create a Java program that will allow the user to register purchases until the user
+decides to exit.
+● Implement a way to keep track of the total revenue for each visiting day. With each
+new purchase, the values will be updated.
+● Implement a purchase command that will register a new purchase.
+○ The day of the week, time and type of the ticket will be specified.
+○ If the chosen period does not match the visiting schedule of the local
+attraction, display the message: 'The purchase is not valid for the chosen
+period.' along with options to select a new period or cancel the purchase.
+● Implement a command to provide the following statistics: the day with the highest
+sales, the day with the lowest sales, the average sales for the entire week, and the
+total number of sales for the week.
+● Implement an exit command that will allow the user to exit the program.
+     */
+
+
+
+}
