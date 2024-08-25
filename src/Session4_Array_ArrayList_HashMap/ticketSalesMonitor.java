0e30a4b3f09c0337807eb719e3238ec30d41c8ea
DaNaCampean <campean.dana@gmail.com>
2024-08-26 00:51:11 +0300

    Done-ticketsMonitor

--- src/Session4_Array_ArrayList_HashMap/ticketSalesMonitor.java
+++ src/Session4_Array_ArrayList_HashMap/ticketSalesMonitor.java
@@ -24,11 +24,10 @@ week and the time of the arrival (e.g. hour) and returns true if the local attra
 open on that time or false otherwise.
 ● Create a method that calculates and returns the price based on the type of ticket
 (child, student, adult, or senior) and the time at which it is purchased.
-
-
 ● Create a Java program that will allow the user to register purchases until the user
 decides to exit.
-● Implement a way to keep track of the total revenue for each visiting day. With each
+
+●Implement a way to keep track of the total revenue for each visiting day. With each
 new purchase, the values will be updated.
 
 ● Implement a purchase command that will register a new purchase.
@@ -36,7 +35,6 @@ new purchase, the values will be updated.
 ○ If the chosen period does not match the visiting schedule of the local
 attraction, display the message: 'The purchase is not valid for the chosen
 period.' along with options to select a new period or cancel the purchase.
-
 
 
 ● Implement a command to provide the following statistics: the day with the highest
@@ -52,14 +50,48 @@ import java.util.Scanner;
 
 public class ticketSalesMonitor {
 
+    //public static int totalSales = 0;
+    public static int sale = 0;
+    //public static HashMap<String, Integer> totalSalesDays = new HashMap<>();
+
+
     public static void main (String args[]) {
+       // Implement a way to keep track of the total revenue for each visiting day. With each
+        //new purchase, the values will be updated.
+        double totalRevenue = 0;
+        HashMap<String, Double> totalRevenuePerDays = new HashMap<>();
+
+        totalRevenuePerDays.put("Monday", 0.0);
+        totalRevenuePerDays.put("Tuesday", 0.0);
+        totalRevenuePerDays.put("Wednesday", 0.0);
+        totalRevenuePerDays.put("Thursday", 0.0);
+        totalRevenuePerDays.put("Friday", 0.0);
+
+        int totalSales = 0;
+
+        HashMap<String, Integer> totalSalesDays = new HashMap<>();
+
+        totalSalesDays.put("Monday", 0);
+        totalSalesDays.put("Tuesday", 0);
+        totalSalesDays.put("Wednesday", 0);
+        totalSalesDays.put("Thursday", 0);
+        totalSalesDays.put("Friday", 0);
+
+
+
+
 
     // Implement the visiting timetable of the local attraction using a Map implementation of
     // your choice in which the keys are days of the week and the values are time intervals (opening and closing hours).
 
         HashMap<String, String> visitingTimeTable = new HashMap<>();
         visitingTimeTable = visitingTimeTable();
-        System.out.println("Table = " +visitingTimeTable);
+        System.out.println("Visiting Program");
+
+        for (String i : visitingTimeTable.keySet()) {
+            System.out.println(i + " : " + visitingTimeTable.get(i));
+        }
+        // System.out.println("Table = " +visitingTimeTable);
 
 
     //Create a boolean method called isOpen that will take as parameters the day of the
@@ -68,43 +100,128 @@ public class ticketSalesMonitor {
       //  String readDay = readPurchaseDay();
 
         Scanner userinput = new Scanner(System.in);
+        boolean continuePurchase = true;
+        double tiketPrice = 0;
+        int salesNumber = 0;
+        do {
 
-        String day = readPurchaseDay(userinput);
-        String time = readPurchaseTime(userinput);
+            String day = readPurchaseDay(userinput);
+            System.out.println("Visiting program for " + day + " is: " + visitingTimeTable.get(day));
+            String time = readPurchaseTime(userinput);
+            String ticketType = readPurchaseTicketType(userinput);
+            //registerPurchase(day, time,ticketType);
 
-        String ticketType = readPurchaseTicketType(userinput);
-        //registerPurchase(day, time,ticketType);
+           // System.out.println("isOpen???? = " + isOpen(day, returnHour(time)));
 
-        //System.out.println("USER INPUT = " + isOpen(day, readPurchaseTime(userinput)));
+            //Create a method that calculates and returns the price based on the type of ticket
+            //        (child, student, adult, or senior) and the time at which it is purchased.
 
+          //  System.out.println("final price = === " + returnPrice(ticketType, returnHour(time)));
 
+            //● Implement a purchase command that will register a new purchase.
+            //○ The day of the week, time and type of the ticket will be specified.
+            //○ If the chosen period does not match the visiting schedule of the local
+            //attraction, display the message: 'The purchase is not valid for the chosen
+            //period.' along with options to select a new period or cancel the purchase.
 
+            tiketPrice = totalRevenuePerDays.get(day);
+            salesNumber = totalSalesDays.get(day);
 
-
-
-
-        //Create a method that calculates and returns the price based on the type of ticket
-        //        (child, student, adult, or senior) and the time at which it is purchased.
-
-        System.out.println("final price = === " + returnPrice(ticketType, time));
-
-        //● Implement a purchase command that will register a new purchase.
-        //○ The day of the week, time and type of the ticket will be specified.
-        //○ If the chosen period does not match the visiting schedule of the local
-        //attraction, display the message: 'The purchase is not valid for the chosen
-        //period.' along with options to select a new period or cancel the purchase.
-
-        registerPurchase(day,returnHour(time),ticketType);
+            totalRevenue = registerPurchase(day,returnHour(time),ticketType, userinput);
 
 
+            tiketPrice = tiketPrice+totalRevenue;
+            salesNumber = salesNumber + sale;
+            sale = 0;
+
+            totalRevenuePerDays.put(day,tiketPrice);
+            totalSalesDays.put(day, salesNumber);
+
+            System.out.println("REVENUE = " + totalRevenuePerDays);
+            System.out.println("SALESSS = " + totalSalesDays);
 
 
+
+
+
+
+            System.out.println("Do you want to add a new purchase? Enter yes to continue, no to exit ");
+
+            String option_scanner = userinput.nextLine();
+
+            if (option_scanner.equals("no"))
+                continuePurchase = false;
+
+
+
+        }while (continuePurchase);
+
+        System.out.println("REVENUE finallll = " + totalRevenuePerDays);
+        System.out.println("Sales  finallll = " + totalSalesDays);
         userinput.close();
 
+        //Implement a command to provide the following statistics: the day with the highest
+        // sales, the day with the lowest sales, the average sales for the entire week, and the
+        // total number of sales for the week.
+
+        System.out.println("GENERATED REPORT:");
+        generated_report(totalSalesDays);
+
+
 
     }
-    public static void registerPurchase (String day, int hour, String ticketType)
+
+    public static void generated_report(HashMap<String, Integer> sales)
     {
+
+        //Implement a command to provide the following statistics: the day with the highest
+        // sales, the day with the lowest sales, the average sales for the entire week, and the
+        // total number of sales for the week.
+
+
+        System.out.println("Sales HashMap = " + sales);
+        for (String i : sales.keySet()) {
+            System.out.println(i + " : " + sales.get(i));
+        }
+
+        int max_sales = sales.get("Monday");
+        int min_sales = sales.get("Monday");
+
+        int total_sales = 0;
+        double average_sales = 0.0;
+
+        int need_var = 0;
+        String need_day_max="Monday";
+        String need_day_min="Monday";
+
+        for (String i : sales.keySet())
+        {
+            need_var = sales.get(i);
+            if (need_var>max_sales)
+                {
+                    max_sales = need_var;
+                    need_day_max = i;
+                }
+            if (need_var<min_sales)
+            {
+                min_sales = need_var;
+                need_day_min = i;
+            }
+
+            total_sales = total_sales + sales.get(i);
+
+
+        }
+        System.out.println("MAX DAY = " + need_day_max + " - sales = " + sales.get(need_day_max));
+        System.out.println("MIN  DAY = " + need_day_min + " - sales = " + sales.get(need_day_min));
+        System.out.println("TOTAL Sales = " + total_sales);
+        average_sales =total_sales/5;
+        System.out.println("AVERAGE SALES = " + average_sales);
+
+
+
+    }
+    public static Double registerPurchase (String day, int hour, String ticketType, Scanner userInput) {
 
         //● Implement a purchase command that will register a new purchase.
         //○ The day of the week, time and type of the ticket will be specified.
@@ -112,25 +229,61 @@ public class ticketSalesMonitor {
         //attraction, display the message: 'The purchase is not valid for the chosen
         //period.' along with options to select a new period or cancel the purchase.
 
-        System.out.println("DAY = " + day);
-        System.out.println("TIME = " + hour);
-        System.out.println("TypeTicket = " + ticketType);
+       // System.out.println("DAY = " + day);
+       // System.out.println("TIME = " + hour);
+       // System.out.println("TypeTicket = " + ticketType);
+       // System.out.println("USER INPUT = " + isOpen(day, hour));
+        boolean retry = true;
+        double price=0;
 
-        System.out.println("Your purchase is: Day = " + day + ", Time = " + hour + ", Type = " + ticketType + ", Price = " + returnPrice(ticketType, hour));
+        do {
 
+            if (isOpen(day, hour)) {
+                price = returnPrice(ticketType, hour);
+                System.out.println("Your purchase is: Day = " + day + ", Time = " + hour + ", Type = " + ticketType + ", Price = " + price);
+                retry = false;
+            } else {
+
+                System.out.println("The purchase is not valid for the chosen period.");
+                System.out.println("Do you want to choose a new period? or cancel the purchase? Enter yes to choose a new time or no to cancel");
+                String option_scanner = userInput.nextLine();
+                if (option_scanner.equals("yes")) {
+                    System.out.println("Enter new hour. Use format HH");
+                    hour = Integer.parseInt(userInput.nextLine());
+
+                } else {
+                    System.out.println("Purchase is canceled");
+                    retry = false;
+                }
+            }
+           // System.out.println("RETRY = "+retry);
+        }while (retry);
+
+        System.out.println("Do you want to proceed with Ticket Purchasing? Enter yes for purchase, no for cancelling the purchase.");
+        String ticketPurchase = userInput.nextLine();
+        if (ticketPurchase.equals("yes")) {
+
+            sale = 1;
+
+        } else {
+            System.out.println("Purchase is canceled");
+            sale =0;
+
+        }
+        return price;
     }
-    public static double returnPrice(String ticketType, String time)
+
+
+    public static double returnPrice(String ticketType, int time)
     {
 
 
         ArrayList<Integer> purchaseTicket = new ArrayList<>();
-        purchaseTicket = convertTime(time);
-        int ora = purchaseTicket.get(0);
-        System.out.println("ORA = "+ora);
-        int min = purchaseTicket.get(1);
+       // purchaseTicket = convertTime(time);
+        int ora = time;
 
         double ticketPricePerTime = ticketPrice(ora);
-        System.out.println("PRET TIKET PE TIMP = " + ticketPricePerTime);
+      //  System.out.println("PRET TIKET PE TIMP = " + ticketPricePerTime);
 
         double finalPrice = 0;
         switch (ticketType)
@@ -154,7 +307,7 @@ public class ticketSalesMonitor {
 
     }
 
-    public static boolean isOpen(String day, String time)
+    public static boolean isOpen(String day, int time)
     {
         //Create a boolean method called isOpen that will take as parameters the day of the
         // week and the time of the arrival (e.g. hour) and returns true if the local attractions is
@@ -166,14 +319,14 @@ public class ticketSalesMonitor {
         HashMap<String, String> visitingTimeTable = new HashMap<>();
         visitingTimeTable = visitingTimeTable();
 
-        System.out.println(" PROGRAM = "+ visitingTimeTable.get(day));
+        //System.out.println(" PROGRAM = "+ visitingTimeTable.get(day));
 
         // ora and Min are the variable for the TicketPurchase time
         int startTime, endTime;
         ArrayList<Integer> purchaseTicket = new ArrayList<>();
-        purchaseTicket = convertTime(time);
-        int ora = purchaseTicket.get(0);
-        int min = purchaseTicket.get(1);
+        //purchaseTicket = convertTime(time);
+        //int ora = purchaseTicket.get(0);
+       // int min = purchaseTicket.get(1);
        // System.out.println("ORA = "+ora);
        // System.out.println("min = "+ min);
 
@@ -191,9 +344,9 @@ public class ticketSalesMonitor {
        // System.out.println("programPerDayMinEnd = "+ programPerDayMinEnd);
 
         Boolean isOpen = false;
-        if ((ora >= programPerDayHourStart) && (ora < programPerDayHourEnd))
+        if ((time >= programPerDayHourStart) && (time < programPerDayHourEnd))
         {
-            System.out.println("MATCH PROGRAMUL");
+          //  System.out.println("MATCH PROGRAMUL");
             isOpen = true;
         }
 
@@ -298,19 +451,19 @@ public class ticketSalesMonitor {
         double pret=0;
         if (hour<=endHourOne)
         {
-            System.out.println("pret = " + priceTimeIntervalOne);
+           // System.out.println("pret = " + priceTimeIntervalOne);
             pret =  priceTimeIntervalOne;
         }
         else
             if ((hour >= startHourTwo) && (hour <= endHourTwo))
         {
-            System.out.println("pret = " + priceTimeIntervalTwo);
+          //  System.out.println("pret = " + priceTimeIntervalTwo);
             pret =   priceTimeIntervalTwo;
         }
             else
             if ((hour >= startHourThree) && (hour <= endHourThree))
             {
-                System.out.println("pret = " + priceTimeIntervalThree);
+              //  System.out.println("pret = " + priceTimeIntervalThree);
                 pret =   priceTimeIntervalThree;
             }
             else
@@ -319,11 +472,7 @@ public class ticketSalesMonitor {
             return pret;
     }
 
-    public static void interval (String time)
-    {
 
-
-    }
 
     public static String readPurchaseTime (Scanner userInput)
     {
@@ -391,11 +540,7 @@ public class ticketSalesMonitor {
         visitingTimeTable.put("Friday", "10:00 - 18:00");
         //System.out.println("table = "+ visitingTimeTable);
 
-        /*
-        System.out.println("Visiting Program");
-        for (String i : visitingTimeTable.keySet()) {
-            System.out.println(i + " : " + visitingTimeTable.get(i));
-        }*/
+
 
         return visitingTimeTable;
 
