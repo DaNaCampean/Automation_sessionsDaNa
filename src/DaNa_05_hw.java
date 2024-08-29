f55cb3cc91f34f0b7d2cc19c7db34d7833f605c9
DaNaCampean <campean.dana@gmail.com>
2024-08-29 18:12:50 +0300

    Edit

---
+++ src/DaNa_05_hw.java
@@ -0,0 +1,40 @@
+public class DaNa_05_hw {
+ /*   Part 5
+
+    Where to learn? Varianta 1 ( part 3.4) Varianta 2 (curs Alex Prodan) Varianta 3 (Udemy – chapters - Primitive Types Recap and the String Data Type) Java Strings & Java String Methods String class & String builder Java Regular Expressions & Pattern class
+
+    Requirements:
+
+            - String operations
+
+    Checkpoint exercise(s) to be solved during meeting:
+
+            1. Write a method reverseString(String str) that takes a string and returns it reversed.
+
+            2. Write a method countVowels(String str) that counts the number of vowels (a, e, i, o, u) in a given string.
+
+            3. Write a method areAnagrams(String str1, String str2) that checks if two given strings are anagrams (contain the same characters in a different order).
+
+            4. Write a method replaceCharacter(String str, char oldChar, char newChar) that replaces all occurrences of oldChar with newChar in the given string.
+
+            5. Write a method isPalindrome(String str) that checks if a given string is a palindrome (reads the same forwards and backwards).
+
+            6. Write a method charFrequency(String str, char c) that takes a string and a character as input and returns the number of times the character appears in the string.
+
+            7. Write a method isTermInList(List<String> list, String term) that checks if the list has an element containing the provided term.
+
+8. Write a method areTermsInList(List<String> list, List<String> searchedTerms) that checks if the list has elements containing the provided terms.
+
+            9. Write a method getFirstInteger(String text) that retrieves the first sequence of digits from a string.
+
+            10. Write a method checkIfTextContainsTerms(String text, boolean ignoreCase, List<String> termsList) that checks if text contains all specified terms with an option to ignore case sensitivity.
+
+11. Write a method insertStringInTextRepeatedly(String text, String stringToBeInserted, int distance) that will insert the given string in the specified text at every specified interval (distance).
+
+            12. Write a method that checks if the given text contains any of the specified terms, with an option to ignore case sensitivity.
+
+13. Write a method that will return a specified number of random elements from a given list.
+}
+
+
+*/
