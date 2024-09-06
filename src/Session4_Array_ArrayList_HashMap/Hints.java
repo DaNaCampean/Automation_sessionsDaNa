cc0d5721d7e34ed83ba499e292b3ece2d2c6471b
DaNaCampean <campean.dana@gmail.com>
2024-09-06 15:28:52 +0300

    Edit ++

--- src/Session4_Array_ArrayList_HashMap/Hints.java
+++ src/Session4_Array_ArrayList_HashMap/Hints.java
@@ -1,6 +1,21 @@
 package Session4_Array_ArrayList_HashMap;
 
+import java.util.Arrays;
+import java.util.ArrayList;
 public class Hints {
+    public static void main (String[] args)
+    {
+        System.out.println("dana");
+        ArrayList<String> myArrayList = new ArrayList<>(Arrays.asList("dana", "conan", "didi","conan", "dana"));
+        for (String s: myArrayList)
+            System.out.print(s + " ");
+
+
+        StringBuilder myStringBuilder = new StringBuilder("DaNa");
+
+
+        System.out.println("Reverse String = " + myStringBuilder.reverse());
+    }
 
   //  multimappppp - pt valuea stuff
    // vezi notele de la probleme....
