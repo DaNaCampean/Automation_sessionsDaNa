a6b99b802b53f755c4bd3c7ee76823c051efb672
DaNaCampean <campean.dana@gmail.com>
2025-04-03 22:49:11 +0300

    mac 3 apra

--- src/selenium/Main.java
+++ src/selenium/Main.java
@@ -1,13 +1,18 @@
 package selenium;
     import org.openqa.selenium.WebDriver;
     import org.openqa.selenium.chrome.ChromeDriver;
+    import org.openqa.selenium.safari.SafariDriver;
+    import org.openqa.selenium.safari.SafariOptions;
 
-    //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
     public class Main {
         public static void main(String[] args) {
-            System.setProperty("webdriver.chrome.driver", "D:\\Backup Softvision\\AUTOMATION STUFF\\selenium\\chromedriver-win64\\chromedriver.exe");
-            WebDriver driver = new ChromeDriver();
+         //   System.setProperty("webdriver.chrome.driver", "/Users/dana/Desktop/JAVA_automation/3apra25/chromedriver-mac-x64/chromedriver");
+           // WebDriver driver = new ChromeDriver();
             //driver.get("https://www.google.com");
+            //SafariOptions options = new SafariOptions();
+            WebDriver driver = new SafariDriver();
+
             driver.navigate().to("https://www.google.com");
             driver.manage().window().maximize();
             String title = driver.getTitle();
