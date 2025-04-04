19060b2f29a1fb7133307fb58b79d5e29468f104
DaNaCampean <campean.dana@gmail.com>
2025-04-04 20:37:48 +0300

    selenium - wind 4apra

--- src/selenium/LearningWebElemenCommands.java
+++ src/selenium/LearningWebElemenCommands.java
@@ -1,5 +1,7 @@
 package selenium;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
@@ -10,9 +12,17 @@ public class LearningWebElemenCommands {
 
         System.setProperty("webdriver.chrome.driver", "D:\\Backup Softvision\\AUTOMATION STUFF\\selenium\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://www-archive.mozilla.org/projects/ui/accessibility/unix/testcase/html/");
 
       //  WebElement firstNameField = driver.findElement(By.id("firstname"));
+        WebElement firstNameInput = driver.findElement(By.id("firstname"));
+        firstNameInput.sendKeys("DaNa");
+
+        WebElement firstNameInput1 = driver.findElement(By.id("lastname"));
+        firstNameInput.clear();
+
+
 
 
       //  driver.quit();
