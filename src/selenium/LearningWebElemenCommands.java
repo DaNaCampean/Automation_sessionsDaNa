225f0aa7199f755ae036ad16bf0e912edfc635cb
DaNaCampean <campean.dana@gmail.com>
2025-04-09 20:07:56 +0300

    selenium - wind

--- src/selenium/LearningWebElemenCommands.java
+++ src/selenium/LearningWebElemenCommands.java
@@ -1,16 +1,19 @@
 package selenium;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.concurrent.TimeUnit;
+
 
 
 public class LearningWebElemenCommands {
 
     public static void main (String[] args) {
 
+
+        System.out.println("STARTTTTTTTTTTTTTTTTTT");
 
         System.setProperty("webdriver.chrome.driver", "D:\\Backup Softvision\\AUTOMATION STUFF\\selenium\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -25,19 +28,86 @@ public class LearningWebElemenCommands {
         firstNameInput1.sendKeys("Didi");
      //  driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
 
-        Thread.sleep(3000);
+
         try{
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-       firstNameInput.clear();
+
+         firstNameInput.clear();
+        //click on element ... text field
+
+        WebElement emailText = driver.findElement(By.id("email"));
+
+       // erroare ????
+
+        WebElement typeHereButton = driver.findElement(By.name("B1"));
+        typeHereButton.click();
+
+
+        try{
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+        // click on link
+
+        // isDisplayed
+
+        //isEnabled
+
+        // isSelected
+        System.out.println("STARTTTTTTTTTTTTTTTTTT3333333333333333");
+
+
+      //  WebElement RadioButton1 = driver.findElement(By.id("rbutton1"));
+        //Radio Button 1
+          WebElement radioButton1 = driver.findElement(By.id("rbutton1"));
+
+        System.out.println("xxxxxxxxxxxxxxxxx");
+        WebElement RadioButton2 = driver.findElement(By.id("rbutton2"));
+      //  System.out.println("RADIO BUTTONS");
+     //  System.out.println("Radio B 1 = " + radioButton1.isSelected());
+       RadioButton2.isSelected();
+        System.out.println("Radio B 2 = " + RadioButton2.isSelected());
+        //Verifying that radioButton1 is selected
+        Boolean isRadioButtonSelected = radioButton1.isSelected();
+        System.out.println("Radio button is selected: " + isRadioButtonSelected);
+        //Verifying that radioButton1 is selected
+       // Boolean isRadioButton2Selected = RadioButton2.isSelected();
+       // System.out.println("Radio button2 is selected: " + isRadioButton2Selected);
+
+//Getting
+
+
+        // submit command
+
+        //getText Command
+
+        //getTagName Command
+
+        //getCssValue Command
+
+        // getAttribute Command
+
+        // getSize Command
+
+        // getLocation Command
+        //
 
 
 
 
 
-       // driver.quit();
+
+
+
+       driver.quit();
 
 
 
