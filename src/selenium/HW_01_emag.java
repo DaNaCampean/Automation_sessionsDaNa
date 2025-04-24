7369448ca9e6ba7e57a52f02f57a4a1a6c764d4a
DaNaCampean <campean.dana@gmail.com>
2025-04-24 15:17:48 +0300

    selenium - wind

--- src/selenium/HW_01_emag.java
+++ src/selenium/HW_01_emag.java
@@ -22,19 +22,27 @@ public class HW_01_emag {
 
 
         System.out.println("STARTTTTTTTTTTTTTTTTTT");
-        System.setProperty("webdriver.chrome.driver", "/Users/dana/Desktop/JAVA_automation/3apra25/chromedriver-mac-x64/chromedriver");
-       //  System.setProperty("webdriver.chrome.driver", "D:\\Backup Softvision\\AUTOMATION STUFF\\selenium\\chromedriver-win64\\chromedriver.exe");
+
+
+        //System.setProperty("webdriver.chrome.driver", "/Users/dana/Desktop/JAVA_automation/3apra25/chromedriver-mac-x64/chromedriver");
+
+        System.setProperty("webdriver.chrome.driver", "D:\\Backup Softvision\\AUTOMATION STUFF\\selenium\\chromedriver-win64\\chromedriver.exe");
+
+
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://emag.ro");
+
         //asserts
 
         String actualTitle = driver.getTitle();
         String expectedTitle = "eMAG.ro - Căutarea nu se oprește niciodată";
-        assertEquals(actualTitle, expectedTitle);
+        assertEquals(actualTitle, expectedTitle, "Verific ca sunt pe partea de start emag"); // doar daca nu e ok
+
         System.out.println("ASSERT OK");
         String expectedTitle1 = "eMAG.ă";
-        assertEquals(actualTitle, expectedTitle);
+        assertEquals(actualTitle, expectedTitle1, "nu sunt pe emag"); // doar daca nu e ok);
+
         System.out.println("ASSERT upsssss");
 
         //  WebElement firstNameField = driver.findElement(By.id("firstname"));
@@ -45,8 +53,10 @@ public class HW_01_emag {
         searchField.submit();
       //  searchField.sendKeys(Keys.RETURN);
 
-// Așteaptă pentru rezultate
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+       // Așteaptă pentru rezultate
+      //  driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+
         // XPATH - search for aspiratoare
         List<WebElement> aspirators = driver.findElements(By.xpath("//div[@class='card-item card-standard js-product-data js-card-clickable']/a"));
 
@@ -64,8 +74,23 @@ public class HW_01_emag {
 
       WebElement addToCart = driver.findElement(By.xpath("//div[@class='flex-shrink-1 flex-grow-1']"));
         addToCart.click();
-        WebElement viewCartFromAlert = driver.findElement(By.xpath("//div[@class='table-cell col-xs-12 col-sm-2 col-md-2 d-none d-md-table-cell']"));
+      /* try{
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+           e.printStackTrace();
+       }
+
+*/
+       // WebElement viewCartFromAlert = driver.findElement(By.xpath("//div[@class='table-cell col-xs-12 col-sm-2 col-md-2 d-none d-md-table-cell']"));
+        WebElement viewCartFromAlert = driver.findElement(By.xpath("//a[@data-dismiss = 'modal']"));
+
         viewCartFromAlert.click();
+        try{
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         WebElement favButton = driver.findElement(By.id("my_wishlist"));
         favButton.click();
 
@@ -78,15 +103,21 @@ public class HW_01_emag {
         WebElement cosulMeu = driver.findElement(By.id("my_cart"));
         cosulMeu.click();
         System.out.println("before FIND");
-       // WebElement stergeElement = driver.findElement(By.xpath("//div[@class='line-item line-item-footer d-md-none']/div[@class='mb-1']/button[@class='btn btn-link outline-0 fs-12 fs-md-14 btn-remove-product gtm_rp080219 remove-product']"));
 
-        WebElement stergeElement = driver.findElement(By.xpath("//div[@class='line-item line-item-footer d-md-none']/div[@class='mb-1']//button[normalize-space(text())='Sterge']"));
+     WebElement stergeElement = driver.findElement(By.xpath("//div[@class='line-item line-item-footer d-none d-md-block']/div[@class='mb-1']/button[@class='btn btn-link outline-0 fs-12 fs-md-14 btn-remove-product gtm_rp080219 remove-product']"));
+
+        //WebElement stergeElement = driver.findElement(By.xpath("/div[@class='line-item line-item-footer d-md-none']/div[@class='mb-1']/button[normalize-space(text())='Sterge']"));
+
+
+
+       // WebElement stergeElement = driver.findElement(By.xpath("//div[@class='line-item line-item-footer d-none d-md-block']//button[@class='btn btn-link outline-0 fs-12 fs-md-14 btn-remove-product gtm_rp080219 remove-product'][normalize-space()='Sterge']"));
+
 
         System.out.println("AFTER FIND, BEFORE CLICK");
 
 
        // WebElement stergeElement = driver.findElement(By.linkText("Sterge"));
-       // stergeElement.click();
+        stergeElement.click();
 
 
 
